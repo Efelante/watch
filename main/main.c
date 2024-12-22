29f3c6bf1d34d621ba7754127447224470aed544
Your Name <you@example.com>
2024-12-23 00:39:53 +0300

    Files are renamed, app works as requiered (right time after wake up)
    TODO: check esp_lcd_panel_disp_on_off() call before deep sleep. If use
    this function the app restarts.
    
    TODO: see esp_lcd_panel_disp_sleep()

--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
+
 #include "sdkconfig.h"
 #include "soc/soc_caps.h"
 #include "freertos/FreeRTOS.h"
@@ -23,7 +24,11 @@
 #include "esp_lvgl_port.h"
 #include "nvs_flash.h"
 #include "nvs.h"
+
 #include "deep_sleep_example.h"
+#include "sntp.h"
+#include "lvgl_watch.h"
+
 
 #if CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
 #include "esp_lcd_sh1107.h"
@@ -31,11 +36,7 @@
 #include "esp_lcd_panel_vendor.h"
 #endif
 
-static void periodic_timer_callback(void* arg);
-
-#include "sntp_example_main.h"
-
-static const char *TAG = "example";
+static const char *TAG = "watch";
 
 #define I2C_HOST  0
 
@@ -60,18 +61,6 @@ static const char *TAG = "example";
 #define EXAMPLE_LCD_CMD_BITS		8
 #define EXAMPLE_LCD_PARAM_BITS		8
 
-extern void example_lvgl_demo_ui(lv_disp_t *disp, const char *str);
-
-/* The LVGL port component calls esp_lcd_panel_draw_bitmap API for send data to the screen. There must be called
-lvgl_port_flush_ready(disp) after each transaction to display. The best way is to use on_color_trans_done
-callback from esp_lcd IO config structure. In IDF 5.1 and higher, it is solved inside LVGL port component. */
-static bool notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
-{
-    lv_disp_t * disp = (lv_disp_t *)user_ctx;
-    lvgl_port_flush_ready(disp);
-    return false;
-}
-
 #if SOC_RTC_FAST_MEM_SUPPORTED
 static RTC_DATA_ATTR struct timeval sleep_enter_time;
 #else
@@ -80,12 +69,87 @@ static struct timeval sleep_enter_time;
 
 static int show_timer = 0;
 
+static void 						periodic_timer_callback(void* arg);
+static void 						deep_sleep_task(void *args);
+static void 						__attribute__((unused)) example_deep_sleep_register_rtc_timer_wakeup(void);
+static void 						i2c_init();
+static esp_lcd_panel_io_handle_t 	install_panel_io(void);
+static esp_lcd_panel_handle_t 		install_ssd1306_panel_driver(esp_lcd_panel_io_handle_t io_handle);
+static bool 						notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx);
+static lv_disp_t* 					lvgl_init(esp_lcd_panel_io_handle_t io_handle, esp_lcd_panel_handle_t panel_handle);
+extern void 						example_lvgl_demo_ui(lv_disp_t *disp, const char *str);
+
+
+void app_main(void)
+{
+	i2c_init();
+
+	esp_lcd_panel_io_handle_t io_handle = install_panel_io();
+
+    esp_lcd_panel_handle_t panel_handle = install_ssd1306_panel_driver(io_handle);
+
+	lv_disp_t *disp = lvgl_init(io_handle, panel_handle);
+
+    // Rotate the screen 
+    lv_disp_set_rotation(disp, LV_DISP_ROT_180);
+
+	// Create a periodic timer which will run every second and update the screen
+    const esp_timer_create_args_t periodic_timer_args = {
+            .callback = &periodic_timer_callback,
+            /* name is optional, but may help identify the timer when debugging */
+            .name = "screen_update_periodic"
+    };
+
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    /* The timer has been created but is not running yet */
+
+	char timebuf[128] = {0};
+
+	char connect_status_str[128] = {0};
+	sprintf(connect_status_str, "%s %s", "Connect to ", CONFIG_EXAMPLE_WIFI_SSID);
+	example_lvgl_demo_ui(disp, connect_status_str);
+	sntp_app_main(timebuf);
+	
+    /* Start the timers */
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000));
+	
+	example_lvgl_demo_ui(disp, timebuf);
+
+    /* Enable wakeup from deep sleep by rtc timer */
+    //example_deep_sleep_register_rtc_timer_wakeup();
+
+#if CONFIG_EXAMPLE_GPIO_WAKEUP
+    /* Enable wakeup from deep sleep by gpio */
+    example_deep_sleep_register_gpio_wakeup();
+#endif
+
+#if CONFIG_EXAMPLE_EXT0_WAKEUP
+    /* Enable wakeup from deep sleep by ext0 */
+    example_deep_sleep_register_ext0_wakeup();
+#endif
+
+#if CONFIG_EXAMPLE_EXT1_WAKEUP
+    /* Enable wakeup from deep sleep by ext1 */
+    example_deep_sleep_register_ext1_wakeup();
+#endif
+
+#if CONFIG_EXAMPLE_TOUCH_WAKEUP
+    /* Enable wakeup from deep sleep by touch */
+    //example_deep_sleep_register_touch_wakeup();
+#endif
+
+    xTaskCreate(deep_sleep_task, "deep_sleep_task", 4096, (void *) &panel_handle, 6, NULL);
+}
+
 static void deep_sleep_task(void *args)
 {
 	char timebuf[128] = {0};
     esp_lcd_panel_handle_t *panel_handle = (esp_lcd_panel_handle_t *) args;
-	sntp_app_main(timebuf);
-	update_label(timebuf);
+
+	//sntp_app_main(timebuf);
+	//update_label(timebuf);
+
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(*panel_handle, true));
 	printf("timebuf: %s\n", timebuf);
     /**
@@ -152,9 +216,9 @@ static void deep_sleep_task(void *args)
             uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
             if (wakeup_pin_mask != 0) {
                 int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
-                printf("Wake up from GPIO %d\n", pin);
+                printf("Wake up from EXT1: pin %d\n", pin);
             } else {
-                printf("Wake up from GPIO\n");
+                printf("Wake up from EXT1\n");
             }
             break;
         }
@@ -186,7 +250,6 @@ static void deep_sleep_task(void *args)
 	while (show_timer < 10) {
 	}
 	show_timer = 0;
-    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(*panel_handle, false));
 
     printf("Entering deep sleep\n");
 
@@ -201,18 +264,25 @@ static void deep_sleep_task(void *args)
     nvs_close(nvs_handle);
 #endif
 
-    // enter deep sleep
+	//TODO: Some bug is here. If uncomment the app restarts
+    //ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(*panel_handle, false));
+	
+	//TODO: see esp_lcd_panel_disp_sleep() that can reduce the power
+	//consumption of the LCD screen by entering the sleep mode. The internal
+	//frame buffer is still retained.
+	
+    //enter deep sleep
     esp_deep_sleep_start();
 }
 
-static void example_deep_sleep_register_rtc_timer_wakeup(void)
+static void __attribute__((unused)) example_deep_sleep_register_rtc_timer_wakeup(void) 
 {
     const int wakeup_time_sec = 20;
     printf("Enabling timer wakeup, %ds\n", wakeup_time_sec);
     ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000));
 }
 
-void app_main(void)
+static void i2c_init()
 {
     ESP_LOGI(TAG, "Initialize I2C bus");
     i2c_config_t i2c_conf = {
@@ -225,7 +295,10 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(i2c_param_config(I2C_HOST, &i2c_conf));
     ESP_ERROR_CHECK(i2c_driver_install(I2C_HOST, I2C_MODE_MASTER, 0, 0, 0));
+}
 
+static esp_lcd_panel_io_handle_t install_panel_io(void)
+{
     ESP_LOGI(TAG, "Install panel IO");
     esp_lcd_panel_io_handle_t io_handle = NULL;
     esp_lcd_panel_io_i2c_config_t io_config = {
@@ -244,9 +317,13 @@ void app_main(void)
 #endif
     };
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)I2C_HOST, &io_config, &io_handle));
+	return io_handle;
+}
 
+static esp_lcd_panel_handle_t install_ssd1306_panel_driver(esp_lcd_panel_io_handle_t io_handle)
+{
     ESP_LOGI(TAG, "Install SSD1306 panel driver");
-    static esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_handle_t panel_handle = NULL;
     esp_lcd_panel_dev_config_t panel_config = {
         .bits_per_pixel = 1,
         .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
@@ -264,7 +341,22 @@ void app_main(void)
 #if CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
     ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
 #endif
+	return panel_handle;
+}
 
+/* The LVGL port component calls esp_lcd_panel_draw_bitmap API for send data to the screen. There must be called
+lvgl_port_flush_ready(disp) after each transaction to display. The best way is to use on_color_trans_done
+callback from esp_lcd IO config structure. In IDF 5.1 and higher, it is solved inside LVGL port component. */
+static bool notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_disp_t * disp = (lv_disp_t *)user_ctx;
+    lvgl_port_flush_ready(disp);
+    return false;
+}
+
+static lv_disp_t* lvgl_init(esp_lcd_panel_io_handle_t io_handle, 
+					 esp_lcd_panel_handle_t panel_handle)
+{
     ESP_LOGI(TAG, "Initialize LVGL");
     const lvgl_port_cfg_t lvgl_cfg = ESP_LVGL_PORT_INIT_CONFIG();
     lvgl_port_init(&lvgl_cfg);
@@ -292,41 +384,7 @@ void app_main(void)
     };
     esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, disp);
 
-    /* Rotation of the screen */
-    lv_disp_set_rotation(disp, LV_DISP_ROT_180);
-
-	// Create a periodic timer which will run every second and update the screen
-    const esp_timer_create_args_t periodic_timer_args = {
-            .callback = &periodic_timer_callback,
-            /* name is optional, but may help identify the timer when debugging */
-            .name = "screen_update_periodic"
-    };
-
-    esp_timer_handle_t periodic_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-    /* The timer has been created but is not running yet */
-
-	char timebuf[128] = {0};
-
-	char connect_status_str[128] = {0};
-	sprintf(connect_status_str, "%s %s", "Connect to ", CONFIG_EXAMPLE_WIFI_SSID);
-	example_lvgl_demo_ui(disp, connect_status_str);
-	sntp_app_main(timebuf);
-	
-    /* Start the timers */
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000));
-	
-	example_lvgl_demo_ui(disp, timebuf);
-
-    /* Enable wakeup from deep sleep by rtc timer */
-    //example_deep_sleep_register_rtc_timer_wakeup();
-
-#if CONFIG_EXAMPLE_GPIO_WAKEUP
-    /* Enable wakeup from deep sleep by gpio */
-    example_deep_sleep_register_gpio_wakeup();
-#endif
-
-    xTaskCreate(deep_sleep_task, "deep_sleep_task", 4096, (void *) &panel_handle, 6, NULL);
+	return disp;
 }
 
 static void periodic_timer_callback(void* arg)
@@ -334,7 +392,12 @@ static void periodic_timer_callback(void* arg)
     int64_t time_since_boot = esp_timer_get_time();
     ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
 	char timebuf[128] = {0};
-	sntp_app_main(timebuf);
+	if (show_timer < 8) {
+		sntp_app_main(timebuf);
+	} else {
+		timebuf[0] = '\0';
+	}
 	update_label(timebuf);
 	show_timer++;
 }
+
--- main/sntp.h
+++ main/sntp.h
@@ -2,6 +2,5 @@
 #define _SNTP_EXAMPLE_H_
 
 void sntp_app_main(char *outtimebuf);
-void update_label(char *outtimebuf);
 
 #endif //_SNTP_EXAMPLE_H_
